0669500083c62cbb2abc22d93fd653fcb80e6eb2
the-s-vector <sebastianjamesturner@outlook.com>
2024-09-05 17:50:07 +0100

    chirp part 2

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,15 +1,21 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725546325',resources: [
+    cacheName: 'chirpy-1725554975',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
 
       
       
-    ],denyPaths: [
-      
     ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
     purge: false
   
 };
