e2f6cdcccb387650f0d141f1213e79a9fda44375
the-s-vector <sebastianjamesturner@outlook.com>
2024-09-22 22:19:40 +0100

    more

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1727039955',resources: [
+    cacheName: 'chirpy-1727039976',resources: [
       '/the-s-vector.eu/assets/css/jekyll-theme-chirpy.css',
       '/the-s-vector.eu/',
       
